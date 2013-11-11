69d3e82cdcab90ee51431d20f486a848e9c5f407
Hongyi Xin <hxin@sloth.ece.cmu.edu>
2013-11-10 20:16:37 -0500

    Added vector_filter.h and .c

---
+++ avx/vector_ed.c
@@ -0,0 +1,14 @@
+/*
+ * vector_ed.c
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: hxin
+ */
+
+#include "vector_ed.h"
+#include <nmmintrin.h>
+
+__m128i shift_right(__m128i pri_vec, __m128i vec, int shift_num) {
+	__m128i result;
+
+}
---
+++ avx/vector_ed.h
@@ -0,0 +1,10 @@
+/*
+ * vector_ed.h
+ *
+ *  Created on: Nov 7, 2013
+ *      Author: hxin
+ */
+
+#include <stdint.h>
+
+
