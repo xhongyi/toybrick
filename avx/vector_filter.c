e487da2a3c2fcb7e65a7b85cc84235a487cc436a
Hongyi Xin <hxin@sloth.ece.cmu.edu>
2013-11-11 20:45:19 -0500

    Finished rightshift program

--- avx/vector_filter.c
+++ avx/vector_filter.c
@@ -8,7 +8,23 @@
 #include "vector_ed.h"
 #include <nmmintrin.h>
 
-__m128i shift_right(__m128i pri_vec, __m128i vec, int shift_num) {
-	__m128i result;
+__m128i shift_right_sse(__m128i pri_vec, __m128i vec, int shift_num) {
+	if (shift_num % 4 == 0)
+		return __m128i _mm_aligner_epi8(pri_vec, vec, 16 - shift_num / 4);
 
+	__m128i carryover;
+	__m128i shiftee;
+	__m128i mask;
+
+	carryover = _mm_aligner_epi8(pri_vec, vec, 15 - shift_num / 4);
+	carryover = _mm_slli_epi64(carryover, (4 - shift_num % 4) * 2);
+
+	if (shift_num > 4)
+		shiftee = _mm_aligner_epi8(pri_vec, vec, 15 - shift_num / 4);
+	else
+		shiftee = vec;
+
+	shiftee = _mm_srli_epi64(shiftee, (shift_num % 4) * 2);
+
+	return _mm_or_si128(shiftee, carryover);
 }
