b8c2f838fc9a4ccb23824c99659b52507cf8bb67
Hongyi Xin <hxin@cmu.edu>
2013-12-02 16:22:37 -0500

    Code in progress

--- avx/vector_filterMain.c
+++ avx/vector_filterMain.c
@@ -5,6 +5,8 @@
  *      Author: hxin
  */
 
+#ifndef BOOST_PP_IS_ITERATING
+
 //#include "print.h"
 #include <stdint.h>
 #include "vector_filter.h"
@@ -12,6 +14,31 @@
 #include <string.h>
 #include <nmmintrin.h>
 #include <emmintrin.h>
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/iteration.hpp>
+
+#define SSE_BIT_LENGTH		128
+#define BASE_SIZE			2
+#define SSE_BASE_NUM		BOOST_PP_DIV(SSE_BIT_LENGTH, BASE_SIZE)
+#define BYTE_BASE_NUM		BOOST_PP_DIV(8, BASE_SIZE)
+#define SSE_BYTE_NUM		BOOST_PP_DIV(SSE_BIT_LENGTH, 8)
+
+MASK_SSE_END[SSE_BIT_LENGTH * SSE_BYTE_NUM] = {
+
+#define BOOST_PP_ITERATION_LIMITS	(0, SSE_BIT_LENGTH - 1)
+#define BOOST_PP_FILENAME_1			"vector_filterMain.c" // this file
+#include BOOST_PP_ITERATE()
+
+#else // BOOST_PP_IS_ITERATING
+
+#define I BOOST_PP_ITERATION()
+#define PRINT_DATA(z, n, data) data
+
+		BOOST_PP_ENUM(SSE_BYTE_NUM, PRINT_DATA, I)
+
+#endif
+#ifndef BOOST_PP_IS_ITERATING
+};
 
 #define _MAX_LENGTH_ 320
 
@@ -42,6 +69,14 @@ int main(int argc, char* argv[]) {
 	if (argc >= 5)
 		average_loc = atoi(argv[4]);
 
+	for (int i = 0; i < SSE_BIT_LENGTH * SSE_BYTE_NUM; i++) {
+		if (i % SSE_BYTE_NUM == 0)
+			printf("\n");
+		printf("%x ", MASK_SSE_END[i]);
+	}
+
+	printf("\n");
+
 //	while (repeat_count--)
 //		bit_vec_filter_sse_simulate(read_t, ref_t, length, error, average_loc);
 	if (bit_vec_filter_sse(read_t, ref_t, length, error))
@@ -52,3 +87,5 @@ int main(int argc, char* argv[]) {
 	return 0;
 
 }
+
+#endif
