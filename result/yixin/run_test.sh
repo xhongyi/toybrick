b8b3ff1466905808e011995bd1fd9e35cd4d548d
Yixin Luo <yixinluo@atom.ece.cmu.edu>
2013-12-16 13:16:33 -0500

    runtest

--- result/yixin/run_test.sh
+++ result/yixin/run_test.sh
@@ -7,6 +7,6 @@ pushd ../../avx
 make test_modifier
 popd
 
-for injected_error in {1,2,3,4,5,6,8,10,15,20,25,30,40,50}; do
+for injected_error in {1,2,3,4,5,6,8,10,15,20,25,30,35,40,45,50}; do
 	../../avx/test_modifier 128 $e $injected_error $f > ./func${f}.err${e}.inj$injected_error.txt
 done
